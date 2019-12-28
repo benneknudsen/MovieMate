31014b3b2a5bb4a0a5a4555018b5891f3a9b37af
Benjamin Schou Knudsen <benjaschou12@yahoo.dk>
2019-12-28 17:02:06 +0100

    Mulighed for at fetch fra moviedb API og tilføjet API KEY

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-const menu = document.querySelector(".menu");
-const btn = menu.querySelector(".nav-tgl");
-btn.addEventListener("click", evt => {
-	if (menu.className.indexOf("active") === -1) {
-		menu.classList.add("active");
-	} else {
-		menu.classList.remove("active");
-	}
-});
+// Values
+
+const API_KEY = '5b912386d63e0f1b35005d2704d11c94';
+
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=5b912386d63e0f1b35005d2704d11c94';
+// Elementer fra DOM
+
+const buttonElement = document.querySelector('#search');
+const inputElement = document.querySelector('#inputValue');
+
+
+
+
+buttonElement.onclick = function(event) {
+	event.preventDefault();
+	const value = inputElement.value;
+	const newUrl = url + '&query=' + value;
+
+	fetch(newUrl)
+	  .then((res) => res.json())
+		.then((data) => {
+			console.log('Data:  ', data);
+		})
+		.catch((error) => {
+			console.log('Error: ', error);
+		});
+	console.log('Value: ', value);
+}
