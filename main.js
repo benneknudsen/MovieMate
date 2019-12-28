2720116c8f3fbf717217b02e92651a3bdde8bc34
Benjamin Schou Knudsen <benjaschou12@yahoo.dk>
2019-12-28 20:18:31 +0100

    Movie posters

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 // Values
 
 const API_KEY = '5b912386d63e0f1b35005d2704d11c94';
-
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 const url = 'https://api.themoviedb.org/3/search/movie?api_key=5b912386d63e0f1b35005d2704d11c94';
 // Elementer fra DOM
 
 const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
+const movieSearchable = document.querySelector('#movies-searchable');
 
 
+function movieSection(movies) {
+	return movies.map((movie) => {
+		return `
+<img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/>
+		`;
+	})}
 
+
+function createMovieContainer(movies) {
+	const movieElement = document.createElement('div');
+	movieElement.setAttribute('class', 'movie');
+
+	const movieTemplate = `
+	<section class="section">
+${movieSection(movies)}
+	</section>
+	<div class="content">
+<p id="content-close">X</p>
+	</div>
+	`;
+
+	movieElement.innerHTML = movieTemplate;
+	return movieElement;
+}
+
+// Movie data i console og skabelse af ny url når man søger
 
 buttonElement.onclick = function(event) {
 	event.preventDefault();
@@ -19,6 +45,10 @@ buttonElement.onclick = function(event) {
 	fetch(newUrl)
 	  .then((res) => res.json())
 		.then((data) => {
+			// Alt film Data
+			const movies = data.results;
+			const movieBlock = createMovieContainer(movies);
+			movieSearchable.appendChild(movieBlock);
 			console.log('Data:  ', data);
 		})
 		.catch((error) => {
